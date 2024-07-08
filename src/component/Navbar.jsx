610cae3bde928c870ceee0cf863653ec14cdb6b9
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2024-07-08 14:57:11 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -13,14 +13,17 @@ const Navbar = ({ setShownav, showNav, count }) => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+  console.log("Hi", count);
   return (
     <motion.div className="grid grid-cols-4 bg-[#3860B2] rounded-sm border mr-2 border-black ">
       <div
         className={`items-center justify-center font-semibold p-2 md:text-[16px] text-[13px] flex border-r border-gray-900 ${
-          count > 1 ? "text-gray-600 bg-gray-300 " : "text-white"
-        } ${count === 1 && "border-t-[5px] border-t-red-500 bg-white text-black"}`}
+          count > 1 && "text-gray-600 bg-gray-300"
+        } ${
+          count === 1 && "border-t-[5px] border-t-red-500 bg-white text-black"
+        }`}
       >
-        {windowwidth >= 900 ? "Business Unit": "BU"}
+        {windowwidth >= 900 ? "Business Unit" : "BU"}
         {count > 1 && (
           <div className="flex items-center ml-2">
             <TiTick className="text-green-600 text-xl" />
@@ -29,8 +32,10 @@ const Navbar = ({ setShownav, showNav, count }) => {
       </div>
       <div
         className={`items-center justify-center font-semibold p-2 md:text-[16px] text-[13px] flex border-r border-gray-900 ${
-          count > 2 ? "text-gray-600 bg-gray-300" : "text-white"
-        } ${count === 2 && "border-t-[5px] border-t-red-500 bg-white text-black"}`}
+          count > 2 && "text-gray-600 bg-gray-300"
+        } ${
+          count === 2 && "border-t-[5px] border-t-red-500 bg-white text-black"
+        }`}
       >
         {windowwidth >= 900 ? "Campaign & Platform" : "Camp & Plat"}
         {count > 2 && (
@@ -41,8 +46,10 @@ const Navbar = ({ setShownav, showNav, count }) => {
       </div>
       <div
         className={`items-center justify-center font-semibold p-2 md:text-[16px] text-[13px] flex border-r border-gray-900 ${
-          count > 3 ? "text-gray-600 bg-gray-300" : "text-white"
-        } ${count === 3 && "border-t-[5px] border-t-red-500 bg-white text-black"}`}
+          count > 3 && "text-gray-600 bg-gray-300"
+        } ${
+          count === 3 && "border-t-[5px] border-t-red-500 bg-white text-black"
+        }`}
       >
         {windowwidth >= 900
           ? "Business & Category & Parameter"
@@ -55,10 +62,12 @@ const Navbar = ({ setShownav, showNav, count }) => {
       </div>
       <div
         className={`items-center justify-center font-semibold p-2 md:text-[16px] text-[13px] flex ${
-          count > 4 ? "text-gray-600 bg-gray-300" : "text-white"
-        } ${count === 4 && "border-t-[5px] border-t-red-500 bg-white text-black"}`}
+          count > 4 && "text-gray-600 bg-gray-300"
+        } ${
+          count === 4 && "border-t-[5px] border-t-red-500 bg-white text-black"
+        }`}
       >
-        {windowwidth >= 900 ? "Campaign Name": "Camp Name"}
+        {windowwidth >= 900 ? "Campaign Name" : "Camp Name"}
         {count > 4 && (
           <div className="flex items-center ml-2">
             <TiTick className="text-green-600 text-xl" />
